05d691ce471abd352169c0948fa56c90e847621a
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-30 22:40:47 +0530

    components

--- 01-react/02-component/src/App.jsx
+++ 01-react/02-component/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
 
   return (
     <div>
+      <Navbar/>
+      <Navbar/>
+      <Card/>
       <Card/>
      
      </div>
