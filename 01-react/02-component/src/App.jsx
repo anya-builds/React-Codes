e71f2bf633d73eb9911cc751dbd1e7e80e916465
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-30 22:21:49 +0530

    gen ai

--- 01-react/02-component/src/App.jsx
+++ 01-react/02-component/src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      App
+     <div className='card'>
+      <h1>Ankita </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit adipisci modi placeat tempora ullam vero dolorum aut necessitatibus laboriosam laudantium assumenda, voluptates unde impedit obcaecati exercitationem ratione aliquid alias consectetur eum. Delectus hic tenetur dolores itaque dolore amet corrupti iure.</p>
+      </div> 
     </div>
   )
 }
