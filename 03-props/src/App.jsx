5881aa374f8f4d7ae0ccc20f02b6ac3294663a41
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-30 23:16:19 +0530

    components

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <div className='parent'>
-     <div className="card">
-      <img src="https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-      <h1>Ankita Arya</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <button>View profile</button>
-     </div>
+     <Card/>
+     <Card/>
     </div>
   )
 }
