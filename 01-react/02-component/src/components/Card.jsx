56581523b48852130c2dad9dfbffb79b4f1baebe
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-30 22:32:44 +0530

    components

--- 01-react/02-component/src/components/Card.jsx
+++ 01-react/02-component/src/components/Card.jsx
@@ -1,6 +1,14 @@
-function Card(){
+import React from 'react'
 
-    return 'hi am Ankita'
+const Card = () => {
+  return (
+    <div>
+     <div className="card">
+        <h1>Ankita Arya</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil iusto nesciunt maxime voluptas quibusdam magni cumque?</p>
+     </div>
+    </div>
+  )
 }
 
 export default Card
